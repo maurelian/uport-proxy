95e46fbe7bd4ebe7f25b366306f8efb3ed6db6e7
Doug A <d11e9@users.noreply.github.com>
2016-01-18 14:50:34 +0000

    add base proxy

---
+++ src/Proxy.sol
@@ -0,0 +1,11 @@
+// The core proxy facade
+// - is owned by a user or implementation contract
+// - only forwards transactions for its owner
+
+import "Owned.sol";
+
+contract Proxy is Owned {
+    function forward(address destination, uint value, bytes data) onlyOwner {
+        destination.call.value(value)(data);
+    }
+}
