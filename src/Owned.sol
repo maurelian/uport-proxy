4b773db2eb07c902971ad1ec521e833b67d205e8
Doug A <d11e9@users.noreply.github.com>
2016-01-18 14:50:55 +0000

    add base owned

---
+++ src/Owned.sol
@@ -0,0 +1,17 @@
+// A base Owned contract
+
+contract Owned {
+    address owner;
+    modifier onlyOwner(){ if (isOwner(msg.sender)) _ }
+    modifier ifOwner(address sender) { if(isOwner(sender)) _ }
+
+    function Owned(){
+        owner = msg.sender;
+    }
+    
+    function isOwner(address addr) public returns(bool) { return addr == owner; }
+
+    function transfer(address _owner) onlyOwner {
+        owner = _owner;
+    }
+}
