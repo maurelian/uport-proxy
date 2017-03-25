df045e2a06c787275e05f20e007b975752177a2b
maurelian <leighvert@protonmail.ch>
2017-03-25 16:41:00 -0400

    Add test network to truffle

--- truffle.js
+++ truffle.js
@@ -4,6 +4,13 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*"
+    }, 
+    test: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*", 
+      gasPrice: 20e9,
+      gas: 0xfffffffffff,
     }
   }
 };
